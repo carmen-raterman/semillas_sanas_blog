ae276e9910108bf02b78dc1bff95d9b93624f507
carmen-raterman <ratersop@gmail.com>
2024-04-30 15:14:12 -0400

    created and implemented edit button for user-specific posts

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { format } from "date-fns";
+import { UserContext } from "../UserContext";
 
 export default function PostPage() {
     const [postInfo, setPostInfo] = useState(null);
+    const {userInfo} = useContext(UserContext);
     const {id} = useParams();
 
     useEffect(() => {
@@ -22,18 +24,41 @@ export default function PostPage() {
       <div className="post-page">
         <h1> {postInfo.title} </h1>
 
-        <time> {format(new Date(postInfo.createdAt), 'MMM d, yyyy')} </time>
-        
+        <time> {format(new Date(postInfo.createdAt), "MMM d, yyyy")} </time>
+
         <div className="author"> by @{postInfo.author.username} </div>
+        {userInfo.id === postInfo.author._id && (
+          <div className="edit-row">
+            <a className="edit-button" href="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+              Edit this post
+            </a>
+          </div>
+        )}
 
         <div className="image">
           <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
 
-        {/* if you want to print HTML from a string, you need to use dangerouslySet in an empty div that is closed directly after */}
+          {/* if you want to print HTML from a string, you need to use dangerouslySet in an empty div that is closed directly after */}
         </div>
 
-        <div className="content" dangerouslySetInnerHTML={{__html: postInfo.content}} />
-
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: postInfo.content }}
+        />
       </div>
     );
 };
