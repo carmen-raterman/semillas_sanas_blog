8d210c421db7d771b24cfe0e39b839286837d322
carmen-raterman <ratersop@gmail.com>
2024-04-30 13:59:19 -0400

    clickable links to single post from post image and post title

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,15 +1,20 @@
 import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Post({title, summary, cover, content, createdAt, author}) {
+export default function Post({_id, title, summary, cover, content, createdAt, author}) {
   //console.log(author)
   return (
     <div className="post">
       <div className="image">
-        <img src= {'http://localhost:4000/'+cover} alt="" />
+        <Link to={`/post/${_id}`}>
+          <img src= {'http://localhost:4000/'+cover} alt="" />
+        </Link>
       </div>
 
       <div className="texts">
-        <h2> {title} </h2>
+        <Link to={`/post/${_id}`}>
+          <h2> {title} </h2>
+        </Link>
         <p className="info">
           <a className="author"> {author ? author.username : 'Anonymous'} </a>
           <time> {format(new Date(createdAt), 'MMM d, yyyy')} </time>
